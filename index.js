08c83164a4c37d58fcf939b69bbe12357ed958cb
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-02-08 03:55:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(arr, fn) {
+    const newArr = [];
+    for (const e of arr) {
+        newArr.push(fn(e));
+    }
+    return newArr;
+}
+
+function reduce(arr, fn, startingPoint) {
+    let val;
+    if (startingPoint !== undefined) {
+        val = startingPoint;
+        for (const e of arr) {
+            val = fn(val, e);
+        }
+    } else {
+        val = arr[0];
+        arr = arr.slice(1);
+        for (const e of arr) {
+            val = fn(val, e);
+        }
+    }
+    return val;
+}
