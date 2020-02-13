b089019df051e335635acb1236ad8b49efa103ef
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-02-13 17:16:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(arr, arr2){
+    // Create an empty array mapArr.
+    const arr3 = [];
+    // Loop through array elements.
+    for(let i=0;i<arr.length;i++){
+        let result = arr[i];
+        arr3.push(arr2(result));
+    }
+    return arr3;
+}
+
+
+function reduce(sourceArray, combine, start=null) {
+    if (start == null) {
+        start = sourceArray[0]
+        for (let i = 1; i < sourceArray.length; i++)
+        start = combine(start, sourceArray[i]);
+    } else{
+    for (let i = 0; i < sourceArray.length; i++)
+      start = combine(start, sourceArray[i])
+    }
+    return start;
+  }
