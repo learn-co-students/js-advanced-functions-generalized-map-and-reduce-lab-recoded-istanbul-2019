c171f1002669859c181638d6a36a1bc2ea0dd51e
mmelihdogan <doganmmelih@gmail.com>
2020-02-15 15:41:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+
+function map(sourceArray, callbackFunction) {
+    let outcomeArray = [];
+    for(let i = 0; i < sourceArray.length; i++) {
+        outcomeArray.push(callbackFunction(sourceArray[i]));
+    }
+
+    return outcomeArray;
+}
+
+function reduce(sourceArray, callbackFunction, startingPoint = undefined) {
+    let sum = startingPoint;
+    if (startingPoint !== undefined) {
+        for (let i = 0; i < sourceArray.length; i++) {
+            const element = sourceArray[i];
+            sum = callbackFunction(element, sourceArray);
+        }
+        return sum;
+    } else {
+        sum = 0;
+        for (let i = 0; i < sourceArray.length; i++) {
+            const element = sourceArray[i];
+            sum = callbackFunction(element, sourceArray);
+        }
+        return sum;
+    }
+}
