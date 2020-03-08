2595743ad33a7f91f8ad06eec54572a530baa39b
abdurrahman rajab <rajab-2@windowslive.com>
2020-03-08 00:28:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Add your functions here
+function map(src, cb) {
+  let retVal = []
+  src.forEach(e =>  retVal.push(cb(e))) 
+  return retVal;
+}
+
+function reduce(src, cb, starting){
+  let retVal = (!starting) ? src[0]:starting 
+  for (let i = (!starting) ? 1 : 0; i < src.length; i++) {
+    retVal = cb(src[i], retVal)
+  }
+  return retVal;
+}
