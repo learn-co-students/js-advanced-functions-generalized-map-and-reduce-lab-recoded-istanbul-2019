13e159a3b3ed6b5e22bd3f3cda4292b126cc1718
yusra <github email address>
2020-03-23 20:12:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(sourceArray, callbackfn) {
+    let newArr = []
+    for (let i = 0; i < sourceArray.length; i++) {
+      let element = sourceArray[i]
+      newArr.push(callbackfn(element))
+    }
+    return newArr;
+  }
+  
+  
+  function reduce(sourceArray, callbackfn, start){
+    if(start == null){
+        return sourceArray.reduce(callbackfn)
+    }
+    else {
+        return sourceArray.reduce(callbackfn, start)
+    }
+}
