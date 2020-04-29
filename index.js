063dc0e558e6a5603c1ef6cc1afd70294f60d132
Omar Alithawi <oadnan100@gmail.com>
2020-04-29 15:36:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+const array = [1,2,3];
+
+function map(array , fnc){
+    let newArray = []
+    for(let i =0; i< array.length; i++){
+        newArray.push(fnc(array[i]));
+    }
+
+    return newArray;
+}
+
+function reduce(array , fnc , str){
+    let newArray = array.reduce(fnc)
+    if(str){
+        return newArray + str;
+    }else{
+        return newArray;
+    }
+    
+}
