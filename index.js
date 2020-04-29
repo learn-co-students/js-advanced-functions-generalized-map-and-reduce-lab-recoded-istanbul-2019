004f6af29241d9be956aa07d76e108d7bbd0c1d0
aydin <aydindavut14@gmail.com>
2020-04-29 18:40:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(soucre, fun) {
+    let newArray = []
+    soucre.forEach(element => {
+        newArray.push(fun(element))
+    });
+    return newArray
+}
+function reduce(soucre, fun, startingPoint) {
+    let i, result;
+    if (startingPoint) {
+        i = 0
+        result = startingPoint
+    }
+    else {
+        i = 1
+        result = soucre[0]
+    }
+    for (; i < soucre.length; i++) {
+        result = fun(soucre[i], result)
+    }
+    return result;
+}
+
+
