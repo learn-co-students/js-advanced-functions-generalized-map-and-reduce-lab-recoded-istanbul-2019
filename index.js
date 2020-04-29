5b705733a50e1cb09719430c2e5115d3a98e08ee
Halit Batur <halitfuatbatur@gmail.com>
2020-04-29 14:59:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+const map = function (array, mapper){
+let newArr=[]
+    for(let i = 0; i < array.length; i++){
+        newArr.push(mapper(array[i]))
+    }
+    return newArr;
+}
+
+const reduce = function(array, reducer, starting = 0){
+    if(starting>0){
+        return array.reduce(reducer)+starting;
+    }else{
+        return array.reduce(reducer);
+    }
+    
+     
+}
