102b49fb2846fc4b8e5a403a95a9bb827eb191d4
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-04-29 20:53:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions here
+function mapToNegativize(Array){
+    let negArr= Array.map(function(element){
+         return -1*element;
+     });
+     return negArr;
+ }
+ function mapToNoChange(Array){
+     let noCh= Array.map(function(element){
+          return element;
+      });
+      return noCh;
+  }
+  function mapToDouble(Array){
+     let double= Array.map(function(element){
+          return element*2;
+      });
+      return double;
+  }
+  function mapToSquare(Array){
+     let square= Array.map(function(element){
+          return element**2;
+      });
+      return square;
+  }
+ 
+ let reduceToTotal = function(sourceArray,startingPoint){
+ 
+     let total=sourceArray.reduce(function (sum,sum1){
+     return sum+sum1;
+ },startingPoint);
+ return total;
+ }
+ 
