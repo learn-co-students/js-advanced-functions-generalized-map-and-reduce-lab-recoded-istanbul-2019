a243fce26a0ed06b4468a075227932a0bf9b77f3
G Bilge Dal <lagala999@gmail.com>
2020-04-30 13:12:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(sourceArray, callback){
+let array = [];
+  for(let i =0;i<sourceArray.length;i++){
+    array.push(callback(sourceArray[i]));
+  }
+return array;
+}
+function reduce(sourceArray, callback, starting){
+  if(starting!=undefined){
+       return sourceArray.reduce(callback)+starting;
+   }
+   return sourceArray.reduce(callback);
+
+
+
+}
