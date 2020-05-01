cd0c45936986f2e113c0d09694fd7b50231294aa
mira_arar <mira.ar230@gmail.com>
2020-05-01 23:37:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+////////////////my own map-like methods/////////////
+const map=(array,fn)=>{
+    let myArray= array.map(fn);
+    return myArray;
+}
+
+//////////////// my own reduce-like methods/////////////
+const reduce=(array,fn,startingPoint=0)=>{
+    let resault;
+    if(startingPoint>0){
+        resault= array.reduce(fn,startingPoint);
+    }
+    else{
+        resault= array.reduce(fn);
+    } 
+    return resault;
+}
+
+
