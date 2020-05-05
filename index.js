69ee137dfa6598209773d85c1e403cd6e9495094
Majd-07 <m.shoghri07@gmail.com>
2020-05-05 13:22:39 +0300

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// let mapToNegativize = function (src) {
-//     let r = []
-//     for (let i = 0; i < src.length; i++) {
-//         r.push(-1 * src[i]) // Unique work
-//     }
-//     return r
-// }
-
-// let mapToNoChange = function (src) {
-//     let r = []
-//     for (let i = 0; i < src.length; i++) {
-//         r.push(src[i]) // Unique work
-//     }
-//     return r
-// }
-
-// function mapToDouble(src) {
-//     let r = []
-//     for (let i = 0; i < src.length; i++) {
-//         r.push(2 * src[i]) // Unique work
-//     }
-//     return r
-// }
-
-// function mapToSquare(src) {
-//     let r = []
-//     for (let i = 0; i < src.length; i++) {
-//         r.push(src[i] * src[i]) // Unique work
-//     }
-//     return r
-// }
 
 function map(src, func) {
     let r = [];
