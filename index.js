d210ac8d54633c99a317209598546c18bac7396f
Burak Selvi <burakselvi858@hotmail.com>
2020-05-06 15:31:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(sourceArray, fn) {
+  let newArr = []
+  sourceArray.forEach(element => {
+    newArr = [...newArr, fn(element)]
+  });
+  return newArr
+}
+
+function reduce(sourceArray, fn, acc = 0) {
+  let total;
+  if (acc) {
+    total = acc;
+    sourceArray.forEach(element => {
+      total = fn(element, total);
+    })
+  } else {
+    total = sourceArray[0];
+    sourceArray.forEach((element, index) => {
+      if (index !== 0) {
+        total = fn(element, total);
+      }
+    })
+  }
+  return total;
+}
