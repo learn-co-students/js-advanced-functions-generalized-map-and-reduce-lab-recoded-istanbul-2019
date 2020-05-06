8c99798ce21d320e2c68d664baf70353e29af8f6
refica <refia.candan@gmail.com>
2020-05-06 12:08:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(sourceArray, fn) {
+    let newArray = [];
+    for (let elements of sourceArray) {
+        newArray.push(fn(elements));
+    };
+    return newArray;
+};
+
+function reduce(sourceArray, fn, startingPoint) {
+    if (startingPoint  !== undefined) {
+        return sourceArray.reduce(fn) + startingPoint;
+    } else {
+        return sourceArray.reduce(fn);
+    };
+};
