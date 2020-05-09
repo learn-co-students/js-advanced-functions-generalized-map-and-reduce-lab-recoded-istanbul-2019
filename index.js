b56cd39764fff0b15bb7d06fa34a4b60c61c3f69
eliftabak <elifchorghay@gmail.com>
2020-05-10 02:14:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(sourceArray, callback) {
+    let newArray = [];
+    for(let i = 0; i < sourceArray.length; ++i) {
+        let number = sourceArray[i];
+        newArray.push(callback(number));
+    }
+    return newArray;
+}
+
+function reduce(sourceArray, callback, start) {
+    if(start == null) {
+        return sourceArray.reduce(callback);
+    } else {
+        return sourceArray.reduce(callback, start);
+    }
+}
