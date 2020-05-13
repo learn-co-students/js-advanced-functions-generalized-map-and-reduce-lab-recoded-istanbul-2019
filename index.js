793e5b3edf9b02290633e441561012da5b46ceba
tugba <ttugbasen@gmail.com>
2020-05-13 16:06:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(array, callback) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, callback, startingPoint) {
+    if(startingPoint != undefined) {
+        return array.reduce(callback) + startingPoint
+    }
+    return array.reduce(callback)
+}
