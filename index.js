c055a1fd9a5e6ce7d7d109544f9875854db19db8
kamalbakour <kamal.bak92@gmail.com>
2020-05-16 21:22:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(array,callback){
+    let newArr=[];
+    
+    for(let element of array){
+      newArr.push(callback(element))
+    }
+    return newArr;
+  }
+  
+  function reduce(array,callback,startingPoint=null){
+    //inside callback we will pass acc curr
+    let value1=startingPoint;
+    
+    if(typeof callback(true,true)=="boolean"){
+       value1 = true;
+    }
+    for(let el of array){
+      value1=callback(value1,el)
+    }
+    
+    
+    return value1;
+  }
