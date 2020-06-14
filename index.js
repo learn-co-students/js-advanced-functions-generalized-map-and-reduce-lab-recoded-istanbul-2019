e9be51d7e8087a507f298107016feb8a286f616f
Dara <dara.dandashi@gmail.com>
2020-06-14 22:51:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, func) {
+    let r = [];
+    for (let i = 0; i < src.length; i++) {
+        r.push(func(src[i]))
+    }
+    return r;
+}
+
+
+function reduce(array, func, startPoint = 0) {
+    let sum = startPoint;
+    if (typeof func(true, true) == 'boolean') {
+        sum = true;
+    }
+    for (let i = 0; i < array.length; i++) {
+        sum = func(sum, array[i]);
+    }
+    return sum;
+};
